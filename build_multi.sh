1c75c8140bf42eb9d128fc9958c7fbe0e0dfe45d
Lee Witek <5556121+0xcafed00d@users.noreply.github.com>
2020-03-07 21:52:49 +0000

    updated multibuild script

--- build_multi.sh
+++ build_multi.sh
@@ -3,9 +3,10 @@
 mkdir picomerge_builds -p
 
 cp README.md picomerge_builds
-env GOOS=windows GOARCH=amd64 go build -o picomerge_builds/picomerge.exe main.go
-env GOOS=linux GOARCH=amd64 go build -o picomerge_builds/picomerge_linux_64 main.go
-env GOOS=linux GOARCH=arm go build -o picomerge_builds/picomerge_linux_arm main.go
-env GOOS=darwin GOARCH=amd64 go build -o picomerge_builds/picomerge_macos_64 main.go
+env GOOS=linux GOARCH=amd64 go build -o picomerge_builds/linux_64bit/picomerge main.go
+env GOOS=linux GOARCH=arm go build -o picomerge_builds/linux_arm/picomerge main.go
+env GOOS=darwin GOARCH=amd64 go build -o picomerge_builds/mac_64bit/picomerge main.go
+env GOOS=windows GOARCH=386 go build -o picomerge_builds/win/picomerge.exe main.go
 
-zip picomerge_builds.zip picomerge_builds/* 
+rm picomerge_builds.zip
+zip -r picomerge_builds.zip picomerge_builds 
